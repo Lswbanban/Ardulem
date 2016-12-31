8fc65717be7aa5cbd42650dba5a4940b2ff259e5
Alban (Banban) Nanty <Alban (Banban) Nanty@localhost>
2017-01-01 02:33:17 +0800

    | first draft of the Map Data

---
+++ MapData.cpp
@@ -0,0 +1,46 @@
+#include <avr/pgmspace.h>
+#include "MapData.h"
+
+// The LOCID macro is used to pack two local id of the SpriteLocalIdList
+#define LOCID(id1, id2) (id1 | (id2<<4))
+
+/*
+ * All the sprite that can be used to create the maps (up to 256, because after that the remaping table cannot handle it)
+ */
+const unsigned char MapData::MapSprite[][8] PROGMEM = {
+	{0x3, 0x7, 0xfd, 0xaf, 0x57, 0xfd, 0x7, 0x3, }, {0x00, 0x00, 0xff, 0xaa, 0x55, 0xff, 0x00, 0x00, }, {0xc0, 0xe0, 0xbf, 0xea, 0xf5, 0xbf, 0xe0, 0xc0, }, {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, },
+};
+
+/*
+ * The various remaping table, to give variety of style to differents levels
+ * Remaping table should have 16 chars.
+ */
+const unsigned char MapData::PillarWorldRemapTable[] PROGMEM = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+
+/*
+ * The various map localization and id list
+ */
+const unsigned char Map01Loca[] PROGMEM = { 0xe0, 0xc0, 0x80, 0x80, 0x80, 0x80, 0x80, 0xf0, 0xf0, 0x80, 0x80, 0xe0, };
+const unsigned char Map01IdList[] PROGMEM = {
+	LOCID(0,1), LOCID(3,1), LOCID(3,3), LOCID(3,3), LOCID(3,3), LOCID(0,2), LOCID(1,3), LOCID(0,2), LOCID(1,3),
+	LOCID(3,3), LOCID(0,1), LOCID(3,0) };
+
+/*
+ * The various maps
+ */
+const MapData::MapDescription MapData::AllMaps[] PROGMEM = {
+	{ // -------- LEVEL 01 -----------
+		10, //StartX
+		10, //StartY
+		80, //HomeX
+		50, //HomeY
+		20, //TimeInMultipleOf10s
+		1, //MinDropSpeed
+		100, //AvailableLemCount
+		10, //RequiredLemCount
+		0xFFFF, //LemButtonConfig
+		(const unsigned char *)MapData::PillarWorldRemapTable, //StriteIDRemapingTable
+		(const unsigned char *)Map01Loca, //SpriteLocalization
+		(const unsigned char *)Map01IdList //SpriteLocalIdList
+	},
+};
---
+++ MapData.h
@@ -0,0 +1,59 @@
+#ifndef MAP_DATA_H
+#define MAP_DATA_H
+/*
+ * This file contains the data of all the map. This include the sprites from which the map is build
+ * but also the each map definition. 
+ */
+namespace MapData
+{
+	// the map sprites are 8x8 pixels
+	extern const unsigned char MapSprite[][8];
+
+	// remaping id table
+	const unsigned int REMAP_TABLE_SIZE = 16;
+	extern const unsigned char PillarWorldRemapTable[];
+
+	struct MapDescription
+	{
+		// the start position where the lems will be spawned
+		char StartX;
+		char StartY;
+		
+		// the position of the home, where the lems will be saved
+		char HomeX;
+		char HomeY;
+		
+		// The time in multiple of 10 seconds
+		char TimeInMultipleOf10s;
+		
+		// the minimum drop speed allowed
+		char MinDropSpeed;
+		
+		// The number of Lem that will be spawned in the Map
+		char AvailableLemCount;
+		
+		// The required number of Lem to save, in order to win
+		char RequiredLemCount;
+		
+		// Lems button configuration (how many lem per type, can you use)
+		unsigned int LemButtonConfig;
+
+		// pointer on one of the remaping table (a 16 sprite subset of all the sprites)
+		const unsigned char * StriteIDRemapingTable;
+
+		// a localization description of the Map. Each char is represent one column of sprite,
+		// each bit indicates if there's a sprite or not. For example, if you want a map of 64x200 pixels
+		// you will need 25 chars
+		const unsigned char * SpriteLocalization;
+		
+		// the list of all the local id (from 0 to 15) of sprite as the set bit appear in the SpriteLocalization
+		// list. These ids should be converted to global ids before being used get the sprite from the MapSprite.
+		// You should use the StriteIDRemapingTable to convert them. Since the local ids only takes 4 bits,
+		// two ids are stored in each char. You can use the Macro LOCID to pack the two ids
+		const unsigned char * SpriteLocalIdList;
+	};
+	
+	// the instance of all the maps
+	extern const MapDescription AllMaps[];
+}
+ #endif
