db5fd3756b826c6a886b0ece61207e31f3a21c29
Alban (Banban) Nanty <Alban (Banban) Nanty@localhost>
2017-10-15 16:24:28 +0800

    | optimize the code size by integrating the Arduboy class inside the project and by deleting unused code. (gain 250 bytes)

--- CustomArduboy.cpp
+++ CustomArduboy.cpp
@@ -1,4 +1,405 @@
 #include "CustomArduboy.h"
+#include "Arduboy_glcdfont.c"
+#include "Arduboy_logo.c"
+
+CustomArduboy::CustomArduboy()
+{
+  // frame management
+  setFrameRate(60);
+  frameCount = 0;
+  nextFrameStart = 0;
+  post_render = false;
+  // init not necessary, will be reset after first use
+  // lastFrameStart
+  // lastFrameDurationMs
+
+  // font rendering  
+  cursor_x = 0;
+  cursor_y = 0;
+  textsize = 1;
+}
+
+void CustomArduboy::begin()
+{
+  boot(); // required
+  bootUtils();
+
+  bootLogo();
+
+  // Audio
+  tunes.initChannel(PIN_SPEAKER_1);
+  tunes.initChannel(PIN_SPEAKER_2);
+  audio.begin();
+}
+
+// this is pusposely duplicated (without logo) so that
+// whichever is actually used is linked and the one
+// that is not is gone without wasting any space in flash
+void CustomArduboy::beginNoLogo()
+{
+  boot(); // required
+  bootUtils();
+
+  // Audio
+  tunes.initChannel(PIN_SPEAKER_1);
+  tunes.initChannel(PIN_SPEAKER_2);
+  audio.begin();
+}
+
+void CustomArduboy::bootUtils()
+{
+  // flashlight
+  if ((buttonsState() & UP_BUTTON) == UP_BUTTON)
+  {
+    // sendLCDCommand(OLED_ALL_PIXELS_ON); // smaller than allPixelsOn()
+    blank();
+    setRGBled(255,255,255);
+    while(true) {}
+  }
+}
+
+void CustomArduboy::bootLogo()
+{
+  // setRGBled(10,0,0);
+  for(int8_t y = -18; y<=24; y++) {
+    setRGBled(24-y, 0, 0);
+
+    clear();
+    drawBitmap(20,y, arduboy_logo, 88, 16, WHITE);
+    display();
+    delay(27);
+    // longer delay post boot, we put it inside the loop to
+    // save the flash calling clear/delay again outside the loop
+    if (y==-16) {
+      delay(250);
+    }
+  }
+
+  delay(750);
+  setRGBled(0,0,0);
+}
+
+/* Frame management */
+
+void CustomArduboy::setFrameRate(uint8_t rate)
+{
+  eachFrameMillis = 1000/rate;
+}
+
+bool CustomArduboy::everyXFrames(uint8_t frames)
+{
+  return frameCount % frames == 0;
+}
+
+bool CustomArduboy::nextFrame()
+{
+  long now = millis();
+
+  // post render
+  if (post_render)
+  {
+#ifdef PROFILE_CPU_LOAD
+    lastFrameDurationMs = now - lastFrameStart;
+#endif
+    frameCount++;
+    post_render = false;
+  }
+
+  // if it's not time for the next frame yet
+  if (now < nextFrameStart)
+  {
+    uint8_t remaining = nextFrameStart - now;
+    // if we have more than 1ms to spare, lets sleep
+    // we should be woken up by timer0 every 1ms, so this should be ok
+    if (remaining > 1)
+      idle();
+    return false;
+  }
+
+  // pre-render
+
+  // technically next frame should be last frame + each frame but if we're
+  // running a slow render we would constnatly be behind the clock
+  // keep an eye on this and see how it works.  If it works well the
+  // lastFrameStart variable could be eliminated completely
+  nextFrameStart = now + eachFrameMillis;
+#ifdef PROFILE_CPU_LOAD
+  lastFrameStart = now;
+#endif
+  post_render = true;
+  return post_render;
+}
+
+#ifdef PROFILE_CPU_LOAD
+int CustomArduboy::cpuLoad()
+{
+  return lastFrameDurationMs*100 / eachFrameMillis;
+}
+#endif
+
+/* Graphics */
+
+void CustomArduboy::clear()
+{
+  fillScreen(BLACK);
+}
+
+void CustomArduboy::drawPixel(int x, int y, uint8_t color)
+{
+  #ifdef PIXEL_SAFE_MODE
+  if (x < 0 || x > (WIDTH-1) || y < 0 || y > (HEIGHT-1))
+  {
+    return;
+  }
+  #endif
+
+  uint8_t row = (uint8_t)y / 8;
+  if (color)
+  {
+    sBuffer[(row*WIDTH) + (uint8_t)x] |=   _BV((uint8_t)y % 8);
+  }
+  else
+  {
+    sBuffer[(row*WIDTH) + (uint8_t)x] &= ~ _BV((uint8_t)y % 8);
+  }
+}
+
+uint8_t CustomArduboy::getPixel(uint8_t x, uint8_t y)
+{
+  uint8_t row = y / 8;
+  uint8_t bit_position = y % 8;
+  return (sBuffer[(row*WIDTH) + x] & _BV(bit_position)) >> bit_position;
+}
+
+uint8_t CustomArduboy::get8PixelsColumn(uint8_t x, uint8_t row)
+{
+  return sBuffer[(row*WIDTH) + x];
+}
+
+void CustomArduboy::drawCircleHelper
+(int16_t x0, int16_t y0, uint8_t r, uint8_t cornername, uint8_t color)
+{
+  int16_t f = 1 - r;
+  int16_t ddF_x = 1;
+  int16_t ddF_y = -2 * r;
+  int16_t x = 0;
+  int16_t y = r;
+
+  while (x<y)
+  {
+    if (f >= 0)
+    {
+      y--;
+      ddF_y += 2;
+      f += ddF_y;
+    }
+
+    x++;
+    ddF_x += 2;
+    f += ddF_x;
+
+    if (cornername & 0x4)
+    {
+      drawPixel(x0 + x, y0 + y, color);
+      drawPixel(x0 + y, y0 + x, color);
+    }
+    if (cornername & 0x2)
+    {
+      drawPixel(x0 + x, y0 - y, color);
+      drawPixel(x0 + y, y0 - x, color);
+    }
+    if (cornername & 0x8)
+    {
+      drawPixel(x0 - y, y0 + x, color);
+      drawPixel(x0 - x, y0 + y, color);
+    }
+    if (cornername & 0x1)
+    {
+      drawPixel(x0 - y, y0 - x, color);
+      drawPixel(x0 - x, y0 - y, color);
+    }
+  }
+}
+
+void CustomArduboy::fillCircleHelper
+(int16_t x0, int16_t y0, uint8_t r, uint8_t cornername, int16_t delta,
+ uint8_t color)
+{
+  // used to do circles and roundrects!
+  int16_t f = 1 - r;
+  int16_t ddF_x = 1;
+  int16_t ddF_y = -2 * r;
+  int16_t x = 0;
+  int16_t y = r;
+
+  while (x < y)
+  {
+    if (f >= 0)
+    {
+      y--;
+      ddF_y += 2;
+      f += ddF_y;
+    }
+
+    x++;
+    ddF_x += 2;
+    f += ddF_x;
+
+    if (cornername & 0x1)
+    {
+      drawFastVLine(x0+x, y0-y, 2*y+1+delta, color);
+      drawFastVLine(x0+y, y0-x, 2*x+1+delta, color);
+    }
+
+    if (cornername & 0x2)
+    {
+      drawFastVLine(x0-x, y0-y, 2*y+1+delta, color);
+      drawFastVLine(x0-y, y0-x, 2*x+1+delta, color);
+    }
+  }
+}
+
+void CustomArduboy::drawRect
+(int16_t x, int16_t y, uint8_t w, uint8_t h, uint8_t color)
+{
+  drawFastHLine(x, y, w, color);
+  drawFastHLine(x, y+h-1, w, color);
+  drawFastVLine(x, y, h, color);
+  drawFastVLine(x+w-1, y, h, color);
+}
+
+void CustomArduboy::drawFastVLine
+(int16_t x, int16_t y, uint8_t h, uint8_t color)
+{
+  int end = y+h;
+  for (int a = max(0,y); a < min(end,HEIGHT); a++)
+  {
+    drawPixel(x,a,color);
+  }
+}
+
+void CustomArduboy::drawFastHLine
+(int16_t x, int16_t y, uint8_t w, uint8_t color)
+{
+  int end = x+w;
+  for (int a = max(0,x); a < min(end,WIDTH); a++)
+  {
+    drawPixel(a,y,color);
+  }
+}
+
+void CustomArduboy::fillRect
+(int16_t x, int16_t y, uint8_t w, uint8_t h, uint8_t color)
+{
+  // stupidest version - update in subclasses if desired!
+  for (int16_t i=x; i<x+w; i++)
+  {
+    drawFastVLine(i, y, h, color);
+  }
+}
+
+void CustomArduboy::fillScreen(uint8_t color)
+{
+  // C version : 
+  //
+  // if (color) color = 0xFF;  //change any nonzero argument to b11111111 and insert into screen array.
+  // for(int16_t i=0; i<1024; i++)  { sBuffer[i] = color; }  //sBuffer = (128*64) = 8192/8 = 1024 bytes. 
+  
+  asm volatile
+  (
+    // load color value into r27
+    "mov r27, %1 \n\t"
+    // if value is zero, skip assigning to 0xff
+    "cpse r27, __zero_reg__ \n\t"
+    "ldi r27, 0xff \n\t"
+    // load sBuffer pointer into Z
+    "movw  r30, %0\n\t"
+    // counter = 0
+    "clr __tmp_reg__ \n\t"
+    "loopto:   \n\t"
+    // (4x) push zero into screen buffer,
+    // then increment buffer position
+    "st Z+, r27 \n\t"
+    "st Z+, r27 \n\t"
+    "st Z+, r27 \n\t"
+    "st Z+, r27 \n\t"
+    // increase counter
+    "inc __tmp_reg__ \n\t"
+    // repeat for 256 loops
+    // (until counter rolls over back to 0)
+    "brne loopto \n\t"
+    // input: sBuffer, color
+    // modified: Z (r30, r31), r27
+    :
+    : "r" (sBuffer), "r" (color)
+    : "r30", "r31", "r27"
+  );
+}
+
+void CustomArduboy::drawRoundRect
+(int16_t x, int16_t y, uint8_t w, uint8_t h, uint8_t r, uint8_t color)
+{
+  // smarter version
+  drawFastHLine(x+r, y, w-2*r, color); // Top
+  drawFastHLine(x+r, y+h-1, w-2*r, color); // Bottom
+  drawFastVLine(x, y+r, h-2*r, color); // Left
+  drawFastVLine(x+w-1, y+r, h-2*r, color); // Right
+  // draw four corners
+  drawCircleHelper(x+r, y+r, r, 1, color);
+  drawCircleHelper(x+w-r-1, y+r, r, 2, color);
+  drawCircleHelper(x+w-r-1, y+h-r-1, r, 4, color);
+  drawCircleHelper(x+r, y+h-r-1, r, 8, color);
+}
+
+void CustomArduboy::fillRoundRect
+(int16_t x, int16_t y, uint8_t w, uint8_t h, uint8_t r, uint8_t color)
+{
+  // smarter version
+  fillRect(x+r, y, w-2*r, h, color);
+
+  // draw four corners
+  fillCircleHelper(x+w-r-1, y+r, r, 1, h-2*r-1, color);
+  fillCircleHelper(x+r, y+r, r, 2, h-2*r-1, color);
+}
+
+void CustomArduboy::drawBitmap
+(int16_t x, int16_t y, const uint8_t *bitmap, uint8_t w, uint8_t h, 
+ uint8_t color)
+{
+  // no need to dar at all of we're offscreen
+  if (x+w < 0 || x > WIDTH-1 || y+h < 0 || y > HEIGHT-1)
+    return;
+
+  int yOffset = abs(y) % 8;
+  int sRow = y / 8;
+  if (y < 0) {
+    sRow--;
+    yOffset = 8 - yOffset;
+  }
+  int rows = h/8;
+  if (h%8!=0) rows++;
+  for (int a = 0; a < rows; a++) {
+    int bRow = sRow + a;
+    if (bRow > (HEIGHT/8)-1) break;
+    if (bRow > -2) {
+      for (int iCol = 0; iCol<w; iCol++) {
+        if (iCol + x > (WIDTH-1)) break;
+        if (iCol + x >= 0) {
+          if (bRow >= 0) {
+            if      (color == WHITE) this->sBuffer[ (bRow*WIDTH) + x + iCol ] |= pgm_read_byte(bitmap+(a*w)+iCol) << yOffset;
+            else if (color == BLACK) this->sBuffer[ (bRow*WIDTH) + x + iCol ] &= ~(pgm_read_byte(bitmap+(a*w)+iCol) << yOffset);
+            else                     this->sBuffer[ (bRow*WIDTH) + x + iCol ] ^= pgm_read_byte(bitmap+(a*w)+iCol) << yOffset;
+          }
+          if (yOffset && bRow<(HEIGHT/8)-1 && bRow > -2) {
+            if      (color == WHITE) this->sBuffer[ ((bRow+1)*WIDTH) + x + iCol ] |= pgm_read_byte(bitmap+(a*w)+iCol) >> (8-yOffset);
+            else if (color == BLACK) this->sBuffer[ ((bRow+1)*WIDTH) + x + iCol ] &= ~(pgm_read_byte(bitmap+(a*w)+iCol) >> (8-yOffset));
+            else                     this->sBuffer[ ((bRow+1)*WIDTH) + x + iCol ] ^= pgm_read_byte(bitmap+(a*w)+iCol) >> (8-yOffset);
+          }
+        }
+      }
+    }
+  }
+}
 
 /*
  * The draw bitmap from RAM is a copy/paste from the DrawBitmap of the Arduboy library instead that it
@@ -44,7 +445,79 @@ void CustomArduboy::drawBitmapFromRAM(int16_t x, int16_t y, const uint8_t *bitma
   }
 }
 
-uint8_t CustomArduboy::get8PixelsColumn(uint8_t x, uint8_t row)
+void CustomArduboy::drawChar
+(int16_t x, int16_t y, unsigned char c, uint8_t color, uint8_t bg, uint8_t size)
 {
-  return sBuffer[(row*WIDTH) + x];
+  boolean draw_background = bg != color;
+
+  if ((x >= WIDTH) ||         // Clip right
+    (y >= HEIGHT) ||        // Clip bottom
+    ((x + 5 * size - 1) < 0) ||   // Clip left
+    ((y + 8 * size - 1) < 0)    // Clip top
+  )
+  {
+    return;
+  }
+
+  for (int8_t i=0; i<6; i++ )
+  {
+    uint8_t line;
+    if (i == 5)
+    {
+      line = 0x0;
+    }
+    else
+    {
+      line = pgm_read_byte(font+(c*5)+i);
+    }
+
+    for (int8_t j = 0; j<8; j++)
+    {
+      uint8_t draw_color = (line & 0x1) ? color : bg;
+
+      if (draw_color || draw_background) {
+        for (uint8_t a = 0; a < size; a++ ) {
+          for (uint8_t b = 0; b < size; b++ ) {
+            drawPixel(x + (i * size) + a, y + (j * size) + b, draw_color);
+          }
+        }
+      }
+      line >>= 1;
+    }
+  }
+}
+
+void CustomArduboy::setCursor(int16_t x, int16_t y)
+{
+  cursor_x = x;
+  cursor_y = y;
+}
+
+void CustomArduboy::setTextSize(uint8_t s)
+{
+  // textsize must always be 1 or higher
+  textsize = s; 
+}
+
+size_t CustomArduboy::write(uint8_t c)
+{
+  if (c == '\n')
+  {
+    cursor_y += textsize*8;
+    cursor_x = 0;
+  }
+  else if (c == '\r')
+  {
+    // skip em
+  }
+  else
+  {
+    drawChar(cursor_x, cursor_y, c, 1, 0, textsize);
+    cursor_x += textsize*6;
+  }
+}
+
+void CustomArduboy::display()
+{
+  this->paintScreen(sBuffer);
 }
--- CustomArduboy.h
+++ CustomArduboy.h
@@ -1,16 +1,154 @@
 #ifndef CUSTOM_ARDUBOY_H
 #define CUSTOM_ARDUBOY_H
 
-#include <Arduboy.h>
+#include "Arduboy_core.h"
+#include <SPI.h>
+#include <Print.h>
+#include <limits.h>
 
-/*
- * This class extend the base Arduboy class to add some custom functions
+/// Library version.
+/**
+ * A version number, 'x.y.z', is stored in the form xxyyzz,
+ * where ((x * 10000) + (y * 100) + (z)),
+ * resulting in 'xxxyyzz', with no leading zeros.
  */
-class CustomArduboy : public Arduboy
+#define ARDUBOY_LIB_VER 10101 // 1.1.1
+
+// EEPROM settings
+#define EEPROM_VERSION 0
+#define EEPROM_BRIGHTNESS 1
+#define EEPROM_AUDIO_ON_OFF 2
+
+// we reserve the first 16 byte of EEPROM for system use
+#define EEPROM_STORAGE_SPACE_START 16 // and onward
+
+// eeprom settings above are neded for audio
+#include "Arduboy_audio.h"
+
+#define PIXEL_SAFE_MODE
+
+// compare Vcc to 1.1 bandgap
+#define ADC_VOLTAGE (_BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1))
+// compare temperature to 2.5 internal reference and _BV(MUX5)
+#define ADC_TEMP (_BV(REFS0) | _BV(REFS1) | _BV(MUX2) | _BV(MUX1) | _BV(MUX0))
+
+class CustomArduboy : public Print, public ArduboyCore
 {
 public:
-	void drawBitmapFromRAM(int16_t x, int16_t y, const uint8_t *bitmap, uint8_t w, uint8_t h, uint8_t color);
-	uint8_t get8PixelsColumn(uint8_t x, uint8_t row);
+  CustomArduboy();
+
+  /// Initializes the hardware
+  void begin();
+  /// Initializes the hardware (but with no boot logo)
+  void beginNoLogo();
+
+  /// Scrolls in the Arduboy logo
+  void bootLogo();
+
+  /// Boot utils such as flashlight, etc
+  void inline bootUtils() __attribute__((always_inline));
+
+  /// Clears display.
+  void clear();
+
+  /// Copies the contents of the screen buffer to the screen.
+  /**
+   * X and Y positions on the display are from the top left corner, thus a Y of 64
+   * is the bottom of the screen and an X of 128 is the right side of the screen.
+   * "Color" or "value" means choosing whether a pixel is lit or not - if color is
+   * 0, the pixel is off (black), if color is 1, the pixel is on (white).
+   */
+  void display();
+
+  /// Sets a single pixel on the screen buffer to white or black.
+  void drawPixel(int x, int y, uint8_t color);
+
+  uint8_t getPixel(uint8_t x, uint8_t y);
+  uint8_t get8PixelsColumn(uint8_t x, uint8_t row);
+
+  /// Draws one or more "corners" of a circle.
+  void drawCircleHelper(int16_t x0, int16_t y0, uint8_t r, uint8_t cornername, uint8_t color);
+
+   /// Draws one or both vertical halves of a filled-in circle.
+  void fillCircleHelper(int16_t x0, int16_t y0, uint8_t r, uint8_t cornername, int16_t delta, uint8_t color);
+
+  /// Draws a rectangle of a width and height.
+  void drawRect(int16_t x, int16_t y, uint8_t w, uint8_t h, uint8_t color);
+
+  /// Draws vertical line.
+  void drawFastVLine(int16_t x, int16_t y, uint8_t h, uint8_t color);
+
+  /// Draws a horizontal line.
+  void drawFastHLine(int16_t x, int16_t y, uint8_t w, uint8_t color);
+
+  /// Draws a filled-in rectangle.
+  void fillRect(int16_t x, int16_t y, uint8_t w, uint8_t h, uint8_t color);
+
+  /// Fills the screen buffer with white or black.
+  void fillScreen(uint8_t color);
+
+  /// Draws a rectangle with rounded edges.
+  void drawRoundRect(int16_t x, int16_t y, uint8_t w, uint8_t h, uint8_t r, uint8_t color);
+
+  /// Draws a filled-in rectangle with rounded edges.
+  void fillRoundRect(int16_t x, int16_t y, uint8_t w, uint8_t h, uint8_t r, uint8_t color);
+
+  /// Draws a bitmap from program memory to a specific X/Y
+  void drawBitmap(int16_t x, int16_t y, const uint8_t *bitmap, uint8_t w, uint8_t h, uint8_t color);
+  void drawBitmapFromRAM(int16_t x, int16_t y, const uint8_t *bitmap, uint8_t w, uint8_t h, uint8_t color);
+
+  /// Draws an ASCII character at a point.
+  void drawChar(int16_t x, int16_t y, unsigned char c, uint8_t color, uint8_t bg, uint8_t size);
+
+  /// Sets the location of the screen cursor.
+  void setCursor(int16_t x, int16_t y);
+
+  /// Set text size
+  /**
+   * As mentioned in drawChar(), individual ASCII characters are 6x8 pixels
+   * (5x7 with spacing on two edges). The size is a pixel multiplier,
+   * so a size of 2 means each character will be 12x16, etc.
+   */
+  void setTextSize(uint8_t s);
+
+  /// Writes a single ASCII character to the screen.
+  virtual size_t write(uint8_t);
+  
+  ArduboyTunes tunes;
+  ArduboyAudio audio;
+
+  void setFrameRate(uint8_t rate);
+  bool nextFrame();
+  bool everyXFrames(uint8_t frames);
+    
+  uint16_t frameCount;
+  uint8_t eachFrameMillis;
+
+#ifdef PROFILE_CPU_LOAD
+  /// Returns the load on the CPU as a percentage.
+  /**
+   * This is based on how much of the time your app is spends rendering
+   * frames.  This number can be higher than 100 if your app is rendering
+   * really slowly.
+   */
+  int cpuLoad();
+
+  uint8_t lastFrameDurationMs;
+  long lastFrameStart;
+#endif
+
+  long nextFrameStart;
+  bool post_render;
+
+protected:
+  unsigned char sBuffer[(HEIGHT*WIDTH)/8];
+
+
+// Adafruit stuff
+protected:
+  uint8_t cursor_x;
+  uint8_t cursor_y;
+  uint8_t textsize;
 };
 
 #endif
