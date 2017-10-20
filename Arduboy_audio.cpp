e4e229d8df112c55fa0cea226a1fadb800dbcca8
Alban (Banban) Nanty <Alban (Banban) Nanty@localhost>
2017-10-20 18:29:48 +0800

    - Optim: remove most of the note frequencies, for the unused note (by music and sfx) - Make the music loop and make the music une only one channel (so that the second one can be used by SFX)   overall it saves 186 bytes

--- Arduboy_audio.cpp
+++ Arduboy_audio.cpp
@@ -19,23 +19,33 @@ volatile unsigned long wait_toggle_count;      /* countdown score waits */
 volatile const byte *score_start = 0;
 volatile const byte *score_cursor = 0;
 
+#define LOWEST_NOTE 52
+#define FIRST_NOTE_IN_INT_ARRAY 52
+#define HIGHEST_NOTE 82
+
 // Table of midi note frequencies * 2
 //   They are times 2 for greater accuracy, yet still fits in a word.
 //   Generated from Excel by =ROUND(2*440/32*(2^((x-9)/12)),0) for 0<x<128
 // The lowest notes might not work, depending on the Arduino clock frequency
 // Ref: http://www.phy.mtu.edu/~suits/notefreqs.html
-const uint8_t PROGMEM _midi_byte_note_frequencies[48] = {
-16,17,18,19,21,22,23,24,26,28,29,31,33,35,37,39,41,44,46,49,52,55,58,62,65,
-69,73,78,82,87,92,98,104,110,117,123,131,139,147,156,165,175,185,196,208,220,
-233,247
+//const uint8_t PROGMEM _midi_byte_note_frequencies[] = {
+//16,17,18,19,21,22,23,24,26,28, // note 0 to 9
+//29,31,33,35,37,39,41,44,46,49, // note 10 to 19
+//52,55,58,62,65,69,73,78,82,87, // note 20 to 29
+//92,98,104,110,117,123,131,139,147,156, // note 30 to 39
+//165,175,185,196,208,220,233,247 // note 40 to 47
+//};
+const unsigned int PROGMEM _midi_word_note_frequencies[] = {
+//262,277,294, //note 48 to 50
+/*311,*/330,349,370,392,415,440,466,494,523, //note 51 to 60
+554,587,622,659,698,740,784,831,880,932, //note 61 to 70
+988,1047,1109,1175,1245,1319,1397,1480,1568,1661, //note 71 to 80
+1760,1865//,1976,2093,2217,2349,2489,2637,2794,2960, //note 81 to 90
+//3136,3322,3520,3729,3951,4186,4435,4699,4978,5274, //note 91 to 100
+//5588,5920,6272,6645,7040,7459,7902,8372,8870,9397, //note 101 to 110
+//9956,10548,11175,11840,12544,13290,14080,14917,15804,16744, //note 111 to 120
+//17740,18795,19912,21096,22351,23680,25088 //note 121 to 127
 };
-const unsigned int PROGMEM _midi_word_note_frequencies[80] = {
-262,277,294,311,330,349,370,392,415,440,466,494,523,554,587,622,659,
-698,740,784,831,880,932,988,1047,1109,1175,1245,1319,1397,1480,1568,1661,1760,
-1865,1976,2093,2217,2349,2489,2637,2794,2960,3136,3322,3520,3729,3951,4186,
-4435,4699,4978,5274,5588,5920,6272,6645,7040,7459,7902,8372,8870,9397,9956,
-10548,11175,11840,12544,13290,14080,14917,15804,16744,17740,18795,19912,21096,
-22351,23680,25088 };
 
 /* AUDIO */
 
@@ -119,17 +129,15 @@ void ArduboyTunes::playNote(byte chan, byte note)
   if (chan >= _tune_num_chans)
     return;
 
-  // we only have frequencies for 128 notes
-  if (note > 127) {
+  // we only have frequencies for a certain range of notes
+  if ((note < LOWEST_NOTE) || (note > HIGHEST_NOTE))
     return;
-  }
 
   timer_num = pgm_read_byte(tune_pin_to_timer_PGM + chan);
-  if (note < 48) {
-    frequency2 = pgm_read_byte(_midi_byte_note_frequencies + note);
-  } else {
-    frequency2 = pgm_read_word(_midi_word_note_frequencies + note - 48);
-  }
+  //if (note < 48)
+  //  frequency2 = pgm_read_byte(_midi_byte_note_frequencies + note - LOWEST_NOTE);
+  //else
+    frequency2 = pgm_read_word(_midi_word_note_frequencies + note - FIRST_NOTE_IN_INT_ARRAY);
 
   //******  16-bit timer  *********
   // two choices for the 16 bit timers: ck/1 or ck/64
